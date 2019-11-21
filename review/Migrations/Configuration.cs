a72c3a63a991169632298b759ab764af99b38ab6
Rushabh27 <45919263+Rushabh27@users.noreply.github.com>
2019-11-21 18:14:45 +0530

    wn

--- review/Migrations/Configuration.cs
+++ review/Migrations/Configuration.cs
@@ -9,8 +9,7 @@ namespace review.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
-            AutomaticMigrationDataLossAllowed = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(review.Models.reviewmodeldb context)
